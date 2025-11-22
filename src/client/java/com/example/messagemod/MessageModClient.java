051676eca6eb950b75f3bca4f95e0daf2aa75ada
DokPlay <serrggeejjj@gmail.com>
2025-11-22 07:36:54 +0300

    Initial commit

---
+++ src/client/java/com/example/messagemod/MessageModClient.java
@@ -0,0 +1,10 @@
+package com.example.messagemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MessageModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
